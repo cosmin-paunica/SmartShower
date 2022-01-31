5c3ea8f1570d02d025211ed465c9a2f1316042ba
Gherasim Rares <rrares33@yahoo.com>
2022-01-31 22:50:15 +0200

    Added routes for WATER

--- water.py
+++ water.py
@@ -10,3 +10,24 @@ def get_db_connection():
     return conn
 
 water = Blueprint('water', __name__)
+
+@water.route('/water', methods=['GET'])
+def get_water_params():
+    conn = get_db_connection()
+    rows = conn.execute('SELECT * FROM water ORDER BY id DESC').fetchone()
+    result = dict(rows)
+    
+    conn.close()
+    return result
+
+@water.route('/water', methods=['PUT'])
+def set_water_params():
+    conn = get_db_connection()
+    data = request.get_json(force=True)
+    new_temp = data['temperature']
+    new_date = data['preparation_date']
+    rows = conn.execute('INSERT INTO water(temperature, preparation_date) VALUES (?,?)', (new_temp, new_date)).fetchall()
+    conn.commit()
+    conn.close()
+    return {'message':'success'}
+
