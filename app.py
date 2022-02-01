cffada55ed19de64dc2ed65f36aae12777f25ba5
raresGabriel01 <baesu_r@yahoo.com>
2022-02-01 18:42:31 +0200

    FIX - added blueprint

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import json
 from users import users
 from dispenser import dispenser
 from water import water
+from quality import quality
 app = Flask(__name__)
 app.register_blueprint(users)
 app.register_blueprint(dispenser)
 app.register_blueprint(water)
+app.register_blueprint(quality)
