15ec82a74efca8d01d7c8722a2f1100a4124435a
raresGabriel01 <baesu_r@yahoo.com>
2022-02-01 19:08:38 +0200

    added start/end shower endpoint

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from dispenser import dispenser
 from water import water
 from quality import quality
 from water_consumption import water_consumption
+from shower import shower
 app = Flask(__name__)
 app.register_blueprint(users)
 app.register_blueprint(dispenser)
 app.register_blueprint(water)
 app.register_blueprint(quality)
 app.register_blueprint(water_consumption)
+app.register_blueprint(shower)
--- shower.py
+++ shower.py
@@ -1,8 +1,11 @@
 import sqlite3
 from subprocess import STARTF_USESTDHANDLES
+from dispenser import use_dispenser
 from flask import Blueprint, request
+from quality import set_quality_info
 
 from users import get_single_user
+from water_consumption import add_consumption
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -11,6 +14,13 @@ def get_db_connection():
 
 shower = Blueprint('shower', __name__)
 
-@shower.routes('/start', methods=['GET'])
+@shower.route('/start', methods=['GET'])
 def start_shower():
-    
+    # consuma din dispenser
+    # pune quality in baza de date
+    use_dispenser()
+    set_quality_info()
+
+@shower.route('/end', methods=['POST'])
+def end_shower():
+    add_consumption()
