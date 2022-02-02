138f735b081541d69387525cf940815f3f39345b
VLCDaniel <vlascenco.daniel@yahoo.com>
2022-02-02 03:22:03 +0200

    login #7

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
-from flask import (Blueprint, request, jsonify)
+import functools
+from flask import (Blueprint, request, jsonify, session, g)
 from db_conn import get_db_connection
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -31,3 +32,50 @@ def register():
         return jsonify({'status': f'User {name} is already registered.'}), 403
 
     return jsonify({'status': 'user registered succesfully'}), 200
+
+@bp.route('/login', methods=["POST"])
+def login():
+    name = request.form['name']
+    password = request.form['password']
+    db = get_db_connection()
+    error = None
+    user = db.execute(
+        'SELECT * FROM users WHERE name = ?', (name,)
+    ).fetchone()
+
+    if user is None:
+        return jsonify({'status': 'user not found'}), 403
+    elif not check_password_hash(user['password'], password):
+        return jsonify({'status': 'password is incorrect'}), 403
+
+    session.clear()
+    session['user_id'] = user['name']
+    return jsonify({'status': 'user logged in succesfully'}), 200
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return jsonify({'status': 'user logged out succesfully'}), 200
+
+
+# !!!!NETESTATE!!!! (they should work tho)
+# def login_required(view):
+#     @functools.wraps(view)
+#     def wrapped_view(**kwargs):
+#         if g.user is None:
+#             return jsonify({'status': 'User is not authenticated'}), 403
+
+#         return view(**kwargs)
+
+#     return wrapped_view
+
+# @bp.before_app_request
+# def load_logged_in_user():
+#     user_id = session.get('user_id')
+
+#     if user_id is None:
+#         g.user = None
+#     else:
+#         g.user = get_db_connection().execute(
+#             'SELECT * FROM users WHERE name = ?', (user_id,)
+#         ).fetchone()
